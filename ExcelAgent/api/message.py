165cd950cd5d78c0b60e9ca585853c195b121749
Ruiyan Zhu <yanny20011102@gmail.com>
2025-11-10 10:08:40 -0800

    refactor: encapsulate in class SingularAgent

--- ExcelAgent/api/message.py
+++ ExcelAgent/api/message.py
@@ -4,15 +4,16 @@ from typing import Dict, Any
 from ..utils.utils import get_logger
 from .schemas import *
 
-from .temp_gemini_api import gemini_one_shot_response
+from .singular_agent import SingularAgent
 from .action_reverse_parse import parse_action_string
-from .action_sequence_sample import sample_dispath
 
 from .exceptions import ActionStrParseError
 
 logger = get_logger(__name__)
 
 router = APIRouter()
+
+singular_agent = SingularAgent()
 
 # Echo message back
 @router.post("/echo", response_model=MessageResponse)
@@ -41,8 +42,8 @@ def process_message(request: SubtaskInstructionRequest):
     # REGEX ^.*$ | SELECT C1:C-1 \n REGEX ^\?.*$ | FORMAT style: backgroundcolor, color: yellow
     # use parse_action_string to parse it into list of actions, ready to be sent to front-end
 
-    #TODO replace the below with calling 3 agent LLMs
-    actions_response = gemini_one_shot_response(subtask_instruction, first_n_rows_of_sheet, read_context)
+    # Optional: replace the below with calling 3 agent LLMs
+    actions_response = singular_agent.singular_agent_response(subtask_instruction, first_n_rows_of_sheet, read_context)
     logger.info("Actions response: " + str(actions_response))
     success = True
     try: 
